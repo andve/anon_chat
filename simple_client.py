838ac5e991bfc6bfc91fc32541c4e3ed2be87de2
andve <106567655+andve@users.noreply.github.com>
2022-06-16 10:33:28 +0000

    Improve command line option parsing with getopt

--- simple_client.py
+++ simple_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import getopt
 import os
 import json
 import sys
@@ -7,7 +8,7 @@ import sys
 import requests
 
 def create_room():
-    return requests.get(f"{url}/create")
+	return requests.get(f"{url}/create")
 
 def send_message(room_code, message):
 	payload = json.dumps({"content": message})
@@ -18,36 +19,69 @@ def retrieve_messages(room_code):
 	return requests.get(f"{url}/chat/{room_code}")
 
 def print_usage():
-	print("usage: simple_client [-c | -s <code> <message> | -r <code>]")
+	print("usage: simple_client [-c | -r <code>] [-s <message> | -g | -h]")
 
-url = os.environ.get("URL") or "http://localhost"
+url = os.environ.get("URL") or "http://localhost:8000"
 
-if len(sys.argv) == 1:
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "cr:m:gh")
+except getopt.GetoptError as err:
+	print(f"error: {err.msg}")
+	sys.exit(2)
+
+if len(opts) == 0:
 	print("error: no options given")
-	print_usage()
-	sys.exit(1)
+	sys.exit()
 
-if sys.argv[1] == "-c":
-	response = create_room()
-	if response.status_code != 200:
-		print("error: room not created")
+g_flag = False
+room_code = ""
+message = ""
+
+for o, a in opts:
+	if o == "-c":
+		try:
+			response = create_room()
+		except requests.exceptions.ConnectionError:
+			print("error: connection failure")
+			sys.exit()
+		if response.status_code != 200:
+			print("error: room not created")
+		else:
+			print(response.text)
+		sys.exit()
+	elif o == "-r":
+		room_code = a
+	elif o == "-m":
+		message = a
+	elif o == "-g":
+		g_flag = True
+	elif o == "-h":
+		print_usage()
+		sys.exit()
 	else:
-		print(response.text)
-elif sys.argv[1] == "-s":
-	if len(sys.argv) < 4:
-		print("error: not enough arguments")
-		sys.exit(1)
-	response = send_message(sys.argv[2], sys.argv[3])
-	if response.status_code == 201:
-		print("message sent")
-	else:
-		print("error: message not sent")
-elif sys.argv[1] == "-r":
-	if len(sys.argv) < 3:
-		print("error: not enough arguments")
-		sys.exit(1)
-	response = retrieve_messages(sys.argv[2])
+		assert False, "unhandled option"
+
+if not room_code:
+	print("error: no room code given")
+	sys.exit()
+
+if g_flag:
+	try:
+		response = retrieve_messages(room_code)
+	except requests.exceptions.ConnectionError:
+		print("error: connection failure")
+		sys.exit()
 	if response.status_code == 404:
 		print("error: invalid code")
 	else:
 		print(response.text)
+else:
+	try:
+		response = send_message(room_code, message)
+	except requests.exceptions.ConnectionError:
+		print("error: connection failure")
+		sys.exit()
+	if response.status_code == 201:
+		print("message sent")
+	else:
+		print("error: message not sent")
